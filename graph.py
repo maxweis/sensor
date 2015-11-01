bd0b639eed9e8b15036b75294a4f132383e1564f
Max Weis <maxrobweis@gmail.com>
2015-11-01 00:03:27 -0500

    cleaner rewrite

--- graph.py
+++ graph.py
@@ -1,89 +1,100 @@
 #!/bin/python2
+
 import sys
 import matplotlib.pyplot as plt
 
-# arg_list = ["--diff", "--mean", "--help"]
+def read_file(path, error="\nFile not available: "):
+    error += path
+    try:
+        with open(path, "r") as open_file:
+            return open_file.read()
+    except IOError:
+        print(error)
+        exit()
 
-def graph_diff(a_y, b_y):
+
+def unpack_coords(text):
+    data = text.split()
+    x_values = map(int, data[::2])
+    y_values = map(int, data[1::2])
+    return (x_values, y_values)
+
+
+def data_mean(data):
+    total = 0
+    n=0
+    for n in data:
+        total += n
+    if n != 0:
+        return total / float(n)
+
+
+def graph_diff(y1, y2):
     output_x = []
     output_y = []
+    if len(y1) > len(y2):
+        max_data = y1
+        min_data = y2
+    else:
+        max_data = y2
+        min_data = y1
 
-    for n in range(len(a_y)):
-        if a_y[n] > b_y[n]:
-            output_y.append(a_y[n] - b_y[n])
-        else:
-            output_y.append(b_y[n] - a_y[n])
+    if len(max_data) != len(min_data):
+        del max_data[len(min_data):]
+
+    high_data = max(min_data, max_data)
+    low_data = min(min_data, max_data)
+
+    for n in range(len(min_data)):
+        output_y.append(high_data[n] - low_data[n])
         output_x.append(n)
-
     return (output_x, output_y)
 
 
-if len(sys.argv) == 2:
-    try:
-        graph_data_name = sys.argv[1]
-        graph_data_file = open(graph_data_name, "r")
-    except IOError:
-        print "\nFile not available :", graph_data_name, "\n"
-        exit()
+help_message = """
+Graph Tool for Sensor Data
+Integers must be stored in text files separated by whitespace (x,y)
 
-    graph_data = graph_data_file.read().split()
-    graph_data_file.close()
+Arguments:
+(none) file           Graph data from file
+--graph file          Graph data from file
+--diff file1 file2    Graph the difference of two data sets stored in files
+--mean file           Return mean of x and y values of 
+"""
 
-    x_values = map(int, graph_data[::2])
-    y_values = map(int, graph_data[1::2])
+if __name__ == "__main__":
+    for arg_n, arg in enumerate(sys.argv[1:]):
+        if arg == "--help":
+            print(help_message)
 
-    plt.plot(x_values, y_values, 'bo')
+        if arg == "--diff":
+            dataA = unpack_coords(read_file(sys.argv[arg_n + 2]))
+            dataB = unpack_coords(read_file(sys.argv[arg_n + 3]))
+            output_graph = graph_diff(dataA[1], dataB[1])
+            plt.plot(output_graph[0], output_graph[1], 'bo')
+            plt.xlabel("samples")
+            plt.ylabel("y")
+            plt.suptitle("Differential graph of '{0}' and '{1}'".format(
+                sys.argv[arg_n + 1], sys.argv[arg_n + 2]))
+            plt.show()
 
-    plt.xlabel("samples")
-    plt.ylabel("y")
-    plt.suptitle("Graph of '" + graph_data_name + "'")
-    plt.show()
+        if  arg == "--mean":
+            data = unpack_coords(read_file(sys.argv[arg_n + 2]))
+            print("x average: {0}\ny average: {1}".format(
+                data_mean(data[0]), data_mean(data[1])))
 
-if "--diff" in sys.argv:
-    argv_index = sys.argv.index("--diff")
-    try:
-        graphA_data_name = sys.argv[argv_index + 1]
-        graphA_data_file = open(graphA_data_name, "r")
-    except IOError:
-        print "\nFile not available :", graphA_data_name, "\n"
-        exit()
+        if arg == "--graph":
+            data = unpack_coords(read_file(sys.argv[arg_n + 2]))
+            plt.plot(data[0], data[1], 'bo')
+            plt.xlabel("samples")
+            plt.ylabel("y")
+            plt.suptitle("Graph of '{0}'".format(sys.argv[arg_n + 2]))
+            plt.show()
 
-    try:
-        graphB_data_name = sys.argv[argv_index + 2]
-        graphB_data_file = open(graphB_data_name, "r")
-    except IOError:
-        print "\nFile not available :", graphB_data_name, "\n"
-        exit()
-
-    graphA_data = graphA_data_file.read().split()
-    graphA_data_file.close()
-
-    graphB_data = graphB_data_file.read().split()
-    graphB_data_file.close()
-
-    graphA_y_values = map(int, graphA_data[1::2])
-
-    graphB_y_values = map(int, graphB_data[1::2])
-
-    output_graph = graph_diff(graphA_y_values, graphB_y_values)
-
-    plt.plot(output_graph[0], output_graph[1], 'bo')
-
-    plt.xlabel("samples")
-    plt.ylabel("y")
-    plt.suptitle("Differential Graph of '" + graphA_data_name + "' and '" + graphB_data_name + "'")
-    plt.show()
-
-if "--mean" in sys.argv:
-    argv_index = sys.argv.index("--mean")
-    graph_length = len(open(sys.argv[argv_index + 1], "r").read())
-    graph_output = [0] * graph_length
-    for n in range(len(sys.argv[argv_index + 1:])):
-        try:
-            graph_name = sys.argv[argv_index + n]
-            graph_file = open(graph_name, "r")
-            graph_data = graph_file.read().split()
-        except IOError:
-            print "\nFile not available :", graph_name, "\n"
-            exit()
-
+        elif len(sys.argv) == 2:
+            data = unpack_coords(read_file(arg))
+            plt.plot(data[0], data[1], 'bo')
+            plt.xlabel("samples")
+            plt.ylabel("y")
+            plt.suptitle("Graph of '{0}'".format(arg))
+            plt.show()
